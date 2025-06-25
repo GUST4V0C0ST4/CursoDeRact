57e18c61752841b8daaee4006e844ab38096425f
GUST4V0C0ST4 <guccosta@gmail.com>
2025-06-25 20:37:45 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react"
+import AddTask from "./componets/AddTask";
+import Tasks from "./componets/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false
+  },
+  {
+    id:2,
+      title: "Estudar ingles",
+      description: "Estudar ingles para se tornar fluente",
+      isCompleted: false
+  },
+  {
+    id:3,
+      title: "Estudar matematica",
+      description: "Estudar matematica para se tornar um desenvolvedor full stack",
+      isCompleted: false
+  },
+]);
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas</h1>        
+        <AddTask />
+        <Tasks tasks={tasks}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
